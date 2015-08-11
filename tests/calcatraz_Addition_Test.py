ba3d4752e8872887f45d30a3011bd33bdbf7588e
Anurag Mishra <a.mishra@syncapse.com>
2015-08-12 00:59:08 +0530

    first commit

---
+++ tests/calcatraz_Addition_Test.py
@@ -0,0 +1,29 @@
+__author__ = 'Anurag'
+
+import Calcatraz_Addition
+from nose.tools import assert_equal
+from nose.tools import assert_not_equal
+from nose.tools import assert_raises
+from nose.tools import raises
+
+class Calcatraz_Addition_Test(object):
+    # @classmethod
+    # def setup_class(klass):
+    #     """This method is run once for each class before any tests are run"""
+    #
+    # @classmethod
+    # def teardown_class(klass):
+    #     """This method is run once for each class _after_ all tests are run"""
+
+    def setUp(self):
+        """This method is run once before _each_ test method is executed"""
+
+    def teardown(self):
+        """This method is run once after _each_ test method is executed"""
+
+
+    def test_addition_two_integer(self):
+        
+
+
+
